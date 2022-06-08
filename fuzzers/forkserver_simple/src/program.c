4c727f4f46ba5d5025b130ae415816392b0e7ae0
tokatoka <tokazerkje@outlook.com>
2022-06-08 22:01:08 +0900

    test-instr.c

--- fuzzers/forkserver_simple/src/program.c
+++ fuzzers/forkserver_simple/src/program.c
@@ -1,35 +1,70 @@
+/*
+   american fuzzy lop++ - a trivial program to test the build
+   --------------------------------------------------------
+   Originally written by Michal Zalewski
+   Copyright 2014 Google Inc. All rights reserved.
+   Copyright 2019-2022 AFLplusplus Project. All rights reserved.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+     https://www.apache.org/licenses/LICENSE-2.0
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-// The following line is needed for shared memeory testcase fuzzing
-__AFL_FUZZ_INIT();
-
-void vuln(char *buf) {
-  if (strcmp(buf, "vuln") == 0) { abort(); }
-}
+#ifdef TEST_SHARED_OBJECT
+  #define main main_exported
+#endif
 
 int main(int argc, char **argv) {
-  FILE *file = stdin;
-  if (argc > 1) { file = fopen(argv[1], "rb"); }
 
-  // The following three lines are for normal fuzzing.
-  /*
-  char buf[16];
-  char* p = fgets(buf, 16, file);
-  buf[15] = 0;
-  */
+  int   fd = 0;
+  char  buff[8];
+  char *buf = buff;
 
-  // The following line is also needed for shared memory testcase fuzzing
-  unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
+  // we support command line parameter and stdin
+  if (argc == 2) {
 
-  printf("input: %s\n", buf);
-  if (buf[0] == 'b') {
-    if (buf[1] == 'a') {
-      if (buf[2] == 'd') { abort(); }
+    buf = argv[1];
+    printf("Input %s - ", buf);
+
+  } else {
+
+    if (argc >= 3 && strcmp(argv[1], "-f") == 0) {
+
+      if ((fd = open(argv[2], O_RDONLY)) < 0) {
+
+        fprintf(stderr, "Error: unable to open %s\n", argv[2]);
+        exit(-1);
+
+      }
+
     }
+
+    if (read(fd, buf, sizeof(buf)) < 1) {
+
+      printf("Hum?\n");
+      return 1;
+
+    }
+
   }
-  vuln(buf);
+
+  // we support three input cases (plus a 4th if stdin is used but there is no
+  // input)
+  if (buf[0] == '0')
+    printf("Looks like a zero to me!\n");
+  else if (buf[0] == '1')
+    printf("Pretty sure that is a one!\n");
+  else
+    printf("Neither one or zero? How quaint!\n");
 
   return 0;
+
 }
